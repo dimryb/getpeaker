06d593028c20d1f4038a3c2cfbfe428763436729
Dmitriy <dimryb@bk.ru>
2021-01-21 00:44:39 +0700

    Более быстрый вариант и вариант с обходом всех 8 точек вокруг

--- qt/getpeaker/getpeaker.h
+++ qt/getpeaker/getpeaker.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
-int16_t getPeak(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD);
+int16_t getPeak1(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD);
+int16_t getPeak2(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD);
+int16_t getPeak3(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD);
 
 #endif // GETPEAKER_H
--- qt/getpeaker/main.cpp
+++ qt/getpeaker/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include <QtDebug>
+#include <QTime>
+#include <QElapsedTimer>
 
 extern "C" {
 #include "getpeaker.h"
@@ -9,8 +11,8 @@ extern "C" {
 
 void simpleTest(){
     enum{
-        r_size = 10,
-        d_size = 10
+        r_size = 1000,
+        d_size = 1000
     };
     int16_t RD[r_size *d_size] = {
         0x0000, 0x0001, 0x0003, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,
@@ -27,11 +29,40 @@ void simpleTest(){
 
     uint32_t outIndexRD[10];
 
-    uint16_t numPeak = getPeak(RD, r_size, d_size, outIndexRD);
+    {
+        QElapsedTimer time;
+        time.start();
+        uint16_t numPeak = getPeak1(RD, r_size, d_size, outIndexRD);
 
-    qDebug() << "number peaks: " << numPeak;
-    for (int i = 0; i < numPeak; ++i){
-        qDebug() << "peak: " << i << " r: " << (outIndexRD[i] >> 16) << " d: " << (outIndexRD[i] & 0xffff);
+        qDebug() << "number peaks: " << numPeak;
+        for (int i = 0; i < numPeak; ++i){
+            qDebug() << "peak: " << i << " r: " << (outIndexRD[i] >> 16) << " d: " << (outIndexRD[i] & 0xffff);
+        }
+        qDebug() << "time: " << time.elapsed();
+    }
+
+    {
+        QElapsedTimer time;
+        time.start();
+        uint16_t numPeak = getPeak2(RD, r_size, d_size, outIndexRD);
+
+        qDebug() << "number peaks: " << numPeak;
+        for (int i = 0; i < numPeak; ++i){
+            qDebug() << "peak: " << i << " r: " << (outIndexRD[i] >> 16) << " d: " << (outIndexRD[i] & 0xffff);
+        }
+        qDebug() << "time: " << time.elapsed();
+    }
+
+    {
+        QElapsedTimer time;
+        time.start();
+        uint16_t numPeak = getPeak3(RD, r_size, d_size, outIndexRD);
+
+        qDebug() << "number peaks: " << numPeak;
+        for (int i = 0; i < numPeak; ++i){
+            qDebug() << "peak: " << i << " r: " << (outIndexRD[i] >> 16) << " d: " << (outIndexRD[i] & 0xffff);
+        }
+        qDebug() << "time: " << time.elapsed();
     }
 }
 
