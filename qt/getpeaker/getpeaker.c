4cbac52b41c0c4cbd5b5dda2904de0f9915e6064
Dmitriy <dimryb@bk.ru>
2021-01-21 23:26:46 +0700

    Новые реализации поиска локальных максимумов

--- qt/getpeaker/getpeaker.c
+++ qt/getpeaker/getpeaker.c
@@ -7,12 +7,20 @@ int16_t getPeak1(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outInd
     int16_t numPeaks = 0;
     for(int16_t r = 0; r < r_size; ++r){
         for(int16_t d = 0; d < d_size; ++d){
+            int16_t val11 = (r > 0)&&(d > 0) ? RD[(r - 1) * r_size + (d - 1)] : 0x8000;
             int16_t val12 = (r > 0) ? RD[(r - 1) * r_size + d] : 0x8000;
+            int16_t val13 = (r > 0)&&(d < d_size - 1) ? RD[(r - 1) * r_size + (d + 1)] : 0x8000;
+
             int16_t val21 = (d > 0) ? RD[r * r_size + (d - 1)] : 0x8000;
             int16_t val22 = RD[r * r_size + d];
             int16_t val23 = (d < d_size - 1) ? RD[r * r_size + (d + 1)] : 0x8000;
+
+            int16_t val31 = (r < r_size - 1)&&(d > 0) ? RD[(r + 1) * r_size + (d - 1)] : 0x8000;
             int16_t val32 = (r < r_size - 1) ? RD[(r + 1) * r_size + d] : 0x8000;
-            if((val22 > val12) && (val22 > val21) && (val22 > val23) && (val22 > val32)){
+            int16_t val33 = (r < r_size - 1)&&(d < d_size - 1) ? RD[(r + 1) * r_size + (d + 1)] : 0x8000;
+            if((val22 > val11) && (val22 > val12) && (val22 > val13) &&
+                    (val22 > val21) && (val22 > val23) &&
+                    (val22 > val31) &&(val22 > val32) &&(val22 > val33)){
                 outIndexRD[numPeaks] = r << 16 | d;
                 ++numPeaks;
             }
@@ -21,32 +29,6 @@ int16_t getPeak1(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outInd
 
     return numPeaks;
 }
-
-int16_t getPeak2(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD){
-    int16_t numPeaks = 0;
-    for(int16_t r = 0; r < r_size; ++r){
-        for(int16_t d = 0; d < d_size; ++d){
-            int16_t val21 = (d > 0) ? RD[r * r_size + (d - 1)] : 0x8000;
-            int16_t val22 = RD[r * r_size + d];
-            if(val22 > val21){
-                int16_t val23 = (d < d_size - 1) ? RD[r * r_size + (d + 1)] : 0x8000;
-                if(val22 > val23){
-                    int16_t val12 = (r > 0) ? RD[(r - 1) * r_size + d] : 0x8000;
-                    if(val22 > val12){
-                        int16_t val32 = (r < r_size - 1) ? RD[(r + 1) * r_size + d] : 0x8000;
-                        if(val22 > val32){
-                            outIndexRD[numPeaks] = r << 16 | d;
-                            ++numPeaks;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    return numPeaks;
-}
-
 
 static const int16_t offsetIndexes[8][2] = {
     {-1, -1}, {-1, 0}, {-1, 1},
@@ -69,6 +51,32 @@ int16_t getPeak3(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outInd
                }else{
                    val = RD[r_sh * r_size + d_sh];
                }
+               if (val22 > val){
+                   ++topCnt;
+               }else{
+                   break;
+               }
+            }
+            if (topCnt == 8){
+                outIndexRD[numPeaks] = r << 16 | d;
+                ++numPeaks;
+            }
+        }
+    }
+
+    return numPeaks;
+}
+
+int16_t getPeak4(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD){
+    int16_t numPeaks = 0;
+    for(int16_t r = 1; r < r_size-1; ++r){
+        for(int16_t d = 1; d < d_size-1; ++d){
+            int16_t val22 = RD[r * r_size + d];
+            int16_t topCnt = 0;
+            for(int16_t i = 0; i < 8; ++i){
+                int16_t r_sh = r + offsetIndexes[i][0];
+                int16_t d_sh = d + offsetIndexes[i][1];
+                int16_t val = RD[r_sh * r_size + d_sh];
                if (val22 > val){
                    ++topCnt;
                }else{
