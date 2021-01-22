5a228c8bd482bc77e1359ab7eb776aa77b99f602
Dmitriy <dimryb@bk.ru>
2021-01-22 18:25:38 +0700

    Новые варианты реализации

--- qt/getpeaker/getpeaker.c
+++ qt/getpeaker/getpeaker.c
@@ -1,23 +1,37 @@
 
 #include "getpeaker.h"
+#include "limits.h"
 
+int16_t getPeak(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD){
+    int16_t numPeaks = 0;
+    // Включить кэш
 
+    // Перенести в статический буфер - быстрее доступ чем с кэша
+
+    // Перенести векторные операции
+        // локальные максимумы если уровень > 10 Дб
+
+    // Использование интринисков
+
+    // + распоралелить по ядрам
+    return numPeaks;
+}
 
 int16_t getPeak1(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outIndexRD){
     int16_t numPeaks = 0;
     for(int16_t r = 0; r < r_size; ++r){
         for(int16_t d = 0; d < d_size; ++d){
-            int16_t val11 = (r > 0)&&(d > 0) ? RD[(r - 1) * r_size + (d - 1)] : 0x8000;
-            int16_t val12 = (r > 0) ? RD[(r - 1) * r_size + d] : 0x8000;
-            int16_t val13 = (r > 0)&&(d < d_size - 1) ? RD[(r - 1) * r_size + (d + 1)] : 0x8000;
+            int16_t val11 = (r > 0)&&(d > 0) ? RD[(r - 1) * r_size + (d - 1)] : SHRT_MIN;
+            int16_t val12 = (r > 0) ? RD[(r - 1) * r_size + d] : SHRT_MIN;
+            int16_t val13 = (r > 0)&&(d < d_size - 1) ? RD[(r - 1) * r_size + (d + 1)] : SHRT_MIN;
 
-            int16_t val21 = (d > 0) ? RD[r * r_size + (d - 1)] : 0x8000;
+            int16_t val21 = (d > 0) ? RD[r * r_size + (d - 1)] : SHRT_MIN;
             int16_t val22 = RD[r * r_size + d];
-            int16_t val23 = (d < d_size - 1) ? RD[r * r_size + (d + 1)] : 0x8000;
+            int16_t val23 = (d < d_size - 1) ? RD[r * r_size + (d + 1)] : SHRT_MIN;
 
-            int16_t val31 = (r < r_size - 1)&&(d > 0) ? RD[(r + 1) * r_size + (d - 1)] : 0x8000;
-            int16_t val32 = (r < r_size - 1) ? RD[(r + 1) * r_size + d] : 0x8000;
-            int16_t val33 = (r < r_size - 1)&&(d < d_size - 1) ? RD[(r + 1) * r_size + (d + 1)] : 0x8000;
+            int16_t val31 = (r < r_size - 1)&&(d > 0) ? RD[(r + 1) * r_size + (d - 1)] : SHRT_MIN;
+            int16_t val32 = (r < r_size - 1) ? RD[(r + 1) * r_size + d] : SHRT_MIN;
+            int16_t val33 = (r < r_size - 1)&&(d < d_size - 1) ? RD[(r + 1) * r_size + (d + 1)] : SHRT_MIN;
             if((val22 > val11) && (val22 > val12) && (val22 > val13) &&
                     (val22 > val21) && (val22 > val23) &&
                     (val22 > val31) &&(val22 > val32) &&(val22 > val33)){
@@ -47,7 +61,7 @@ int16_t getPeak3(int16_t * RD, int16_t r_size, int16_t d_size, uint32_t * outInd
                int16_t d_sh = d + offsetIndexes[i][1];
                int16_t val = 0;
                if ((r_sh < 0)||(d_sh < 0)||(r_sh >= r_size)||(d_sh >= d_size)){
-                   val = 0x8000;
+                   val = SHRT_MIN;
                }else{
                    val = RD[r_sh * r_size + d_sh];
                }
